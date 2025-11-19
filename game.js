8318facee1842dc1dfc6f2a2e34b15ca15091e44
Zizhan Wang <webgoatguy@gmail.com>
2025-11-19 18:53:56 -0500

    commit!

--- game.js
+++ game.js
@@ -1,20 +1,38 @@
 // ----- LEVEL DATA -----
 const LEVELS = {
   easy: [
-    { name: "Easy 1", latex: "f(x)=\\sin(x)" },
-    { name: "Easy 2", latex: "f(x)=x^2" },
-    { name: "Easy 3", latex: "f(x)=\\cos(x)" }
+    { name: "Bounce", latex: "f(x)=\\left|\\ln(x+1)\\right|" },
+    { name: "Wobbly", latex: "f(x)=x^2+\\sin(2x)" },
+    { name: "Jet", latex: "f(x)=\\ln\\left(\\left|2x\\right|\\right)" },
+    { name: "Smooth", latex: "f(x)=x^3-4x" },
+    { name: "Asymptote", latex: "f(x)=2^x-x" },
+    { name: "Slide", latex: "f(x)=\\sin(2x)+x-\\pi" }
   ],
   medium: [
-    { name: "Medium 1", latex: "f(x)=x^3-2x" },
-    { name: "Medium 2", latex: "f(x)=\\sin(x)+\\cos(x)" }
+    { name: "Jittery", latex: "f(x)=\\frac{\\sin(x^2)}{x}" },
+    { name: "Nonchalant", latex: "f(x)=\\frac{\\sin(x)}{x}+\\sqrt{x}+e^{-x}" },
+    { name: "Vertical", latex: "f(x)=\\frac{\\sin(x)}{x^2}+x^2" },
+    { name: "Ouch", latex: "f(x)=x+\\left|\\ln x\\right|-x^2" },
+    { name: "Up Down", latex: "f(x)=e^x+e^{-x}-x^4" },
+    { name: "Boring", latex: "f(x)=\\frac{e^x}{x}" },
+    { name: "Double Bounce", latex: "f(x)=\\left| -\\left|e^x-1\\right|+1 \\right|" },
+    { name: "Moire", latex: "f(x)=\\left| \\sin(x^2)\\sqrt{x} \\right|" },
+    { name: "Familiar", latex: "f(x)=\\ln(x)+e^{-x}" }
   ],
   hard: [
-    { name: "Hard 1", latex: "f(x)=\\sin(2x)+0.5\\cos(3x)" },
-    { name: "Hard 2", latex: "f(x)=x^3-x" }
+    { name: "Diagonal", latex: "f(x)=\\left(1-x^3\\right)^{1/3}" },
+    { name: "Bump", latex: "f(x)=x^(1-x)" },
+    { name: "Checkmark", latex: "f(x)=(\\ln x)^2-e^{-x}" },
+    { name: "Pits", latex: "f(x)=\\ln(\\sin x)" },
+    { name: "Linear", latex: "f(x)=\\frac{\\sin(x)+\\sin(x^2)+\\sin(x^3)}{x}" },
+    { name: "Quadratic", latex: "f(x)=e^{x^2}" },
+    { name: "Up Up And Away", latex: "f(x)=x^{5/2}-e^x+x!" }
   ],
   impossible: [
-    { name: "Impossible 1", latex: "f(x)=\\sin(x^2)" }
+    { name: "Ridiculousness", latex: "f(x)=\\sin(\\sqrt{x})\\sin(x)" },
+    { name: "Polite", latex: "f(x)=x!\\,e^{-x}\\ln(x)" },
+    { name: "Impolite", latex: "f(x)=\\frac{\\sin(x^2)}{x}+x^2\\ln(x)" },
+    { name: "Downright Rude", latex: "f(x)=\\frac{\\ln(1/x)}{x}+e^x+\\tan(x)" }
   ]
 };
 
